6d11e6f4b1cbb3104c50e2a1d2449baccfe86793
BatoBalint <balint.bato@gmail.com>
2021-10-20 17:10:05 +0200

    init kesz

--- app/src/main/java/com/example/kopapirollo/MainActivity.kt
+++ app/src/main/java/com/example/kopapirollo/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.kopapirollo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var UserImage : ImageView
+    lateinit var CpuImage : ImageView
+    lateinit var ScoreBoard : TextView
+    lateinit var ButtonRock : Button
+    lateinit var ButtonPaper : Button
+    lateinit var ButtonScissor : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    fun init() {
+        UserImage = findViewById(R.id.userImage)
+        CpuImage = findViewById(R.id.cpuImage)
+        ScoreBoard = findViewById(R.id.scoreBoard)
+        ButtonRock = findViewById(R.id.btnRock)
+        ButtonPaper = findViewById(R.id.btnPaper)
+        ButtonScissor = findViewById(R.id.btnScissor)
     }
 }
